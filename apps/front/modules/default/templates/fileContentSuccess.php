2039735ab2847a3d4453dadb8477aa651c6e44f3
Maxime AILLOUD <maxime.ailloud@pmsipilot.com>
2011-11-25 15:37:28 +0100

    Adding a page for showing file content with syntax highlighting

---
+++ apps/front/modules/default/templates/fileContentSuccess.php
@@ -0,0 +1,18 @@
+<canvas id="outline" width="50" height="700" style="position: fixed;"></canvas>
+<div class="file_bloc">
+  <div class="data">
+    <div class="data_head">
+      <ul class="right actions">
+        <li>
+          <?php echo link_to('View diff', 'default/file', array('title' => 'View entire file', 'query_string' => 'file='.$file->getId())) ?>
+        </li>
+      </ul>
+    </div>
+    <div class="data_body">
+      <pre class="brush: <?php echo $fileExtension ?>"><?php echo $fileContent; ?></pre>
+  </div>
+</div>
+
+<script type="text/javascript">
+SyntaxHighlighter.all();
+</script>
