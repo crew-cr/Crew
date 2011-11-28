8530afab522b2b779aa9eb3b12a01681bdeb1c60
Denis Roussel <denis.roussel@gmail.com>
2011-11-28 12:16:44 +0100

    add tooltips

--- web/js/context.js
+++ web/js/context.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
   $('#context select').bind('change', function() {
     this.form.submit();
   })
+  $('.tooltip').qtip({
+    style: {
+      classes: 'tooltip'
+    },
+    position: {
+      my: 'top left',
+      target: 'mouse',
+      viewport: $(window), // Keep it on-screen at all times if possible
+      adjust: {
+        x: 10,  y: 10
+      }
+    },
+    hide: {
+      fixed: true // Helps to prevent the tooltip from hiding ocassionally when tracking!
+    }
+  });
 });
