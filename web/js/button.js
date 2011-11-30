fcbf4ad9bced71ae3e81465cdf02a058c7c8844e
Denis Roussel <denis.roussel@gmail.com>
2011-11-30 14:08:15 +0100

    fixed console.log...hum...

--- web/js/button.js
+++ web/js/button.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     if (e.target.nodeName == 'BUTTON') {
       var link = $('a', $(this));
       if (link != undefined){
-        console.log(link);
         $('a', $(this)).trigger('click');
       }
     }
