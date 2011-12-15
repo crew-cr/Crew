676c50d4d67141735fd0944316225ad5067e65ce
Morgan Brunot <morgan.brunot@pmsipilot.com>
2011-12-15 17:20:40 +0100

    improve commentGlobal template

--- apps/front/modules/default/templates/_commentGlobal.php
+++ apps/front/modules/default/templates/_commentGlobal.php
@@ -9,7 +9,12 @@
             <div class="comment_user">
               <img class="avatar" src="<?php echo $commentGlobal->getsfGuardUser()->getProfile()->getAvatarUrl() ?>" />
               <?php echo sprintf("<span><strong>%s</strong> %s</span>", $commentGlobal->getAuthorName(), $commentGlobal->getUpdatedAt('d/m/Y H\hi')) ?>
-              <button class="right"><a title="Copy permalink to clipboard" class="clipboard" href="<?php printf("%s#comment-%s", $_SERVER['REQUEST_URI'], $commentGlobal->getId()) ?>">Permalink</a></button>
+              <?php if(CommentPeer::TYPE_BRANCH === $type): ?>
+                - <?php echo link_to('Permalink', url_for('default/fileList'), array('query_string' => sprintf('branch=%s#comment-%s', $id, $commentGlobal->getId()), 'title' => 'Copy permalink to clipboard', 'class' => 'clipboard')) ?>
+              <?php elseif(CommentPeer::TYPE_FILE === $type): ?>
+                - <?php echo link_to('Permalink', url_for('default/file'), array('query_string' => sprintf('file=%s#comment-%s', $id, $commentGlobal->getId()), 'title' => 'Copy permalink to clipboard', 'class' => 'clipboard')) ?>
+              <?php endif; ?>
+
               <?php echo $userId === $commentGlobal->getUserId() ? sprintf('<button class="right danger delete" data="%s?id=%s&type=%s">Delete</button>', url_for('default/commentDeleteGlobal'), $commentGlobal->getId(), $type) : '' ?>
             </div>
             <div class="comment_body"><?php echo Markdown($commentGlobal->getValue()) ?></div>
@@ -18,7 +23,9 @@
         <?php endif; ?>
         <form name="commentGlobal" id="commentGlobal" class="comment_form" method="post" action="<?php echo url_for('default/commentAddGlobal?id=' . $id . '&type=' . $type) ?>" >
           <?php echo $form['value']->render(); ?>
-          <button class="right icon comment no-marge" type="submit">Add Branch Note</button>
+          <button class="right icon comment no-marge" type="submit">
+            <?php echo $type === CommentPeer::TYPE_BRANCH ? 'Add Branch Note' : 'Add File Note' ?>
+          </button>
         </form>
       </div>
     </td>
