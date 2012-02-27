e7bf6464687750d195a753e4164ac67f8ee221c8
Adrien Gallou <adrien.gallou@pmsipilot.com>
2012-02-27 09:58:58 +0100

    added keyboard shortcuts in file view
      h : previous file
      j : next diff
      k : previous diff
      l : newt file

--- web/js/fileNavigation.js
+++ web/js/fileNavigation.js
@@ -2,16 +2,60 @@ $(document).ready(function() {
 
   var shiftKeyIsPressed = false;
 
+  var scrollToDiff = function(type) {
+    var
+      scrollPosition = $(document).scrollTop(),
+      nextPos = 0,
+      currentPosition = 0,
+      margin = 100
+      ;
+    $(".added, .deleted").each(function() {
+        currentPosition = $(this).offset().top;
+        if (
+            (type == 'bottom' && currentPosition  > (scrollPosition + margin))
+            || (type == 'top' && currentPosition  < (scrollPosition + margin))
+        ) {
+          nextPos = currentPosition;
+          return false;
+        }
+    });
+    if (nextPos > 0) {
+      var position = nextPos - margin;
+      $(document).scrollTop(position);
+    }
+  };
+
+  var changeFile = function(selector) {
+    var location = $(selector).click().first().attr('href');
+    if (typeof location != 'undefined') {
+      window.location = location;
+    }
+  };
+
   $(document)
     .keydown(function(e) {
-      var code = e.keyCode || e.which
-      if (code == '16') {
-        shiftKeyIsPressed = true;
+      var code = e.keyCode || e.which;
+      switch (code) {
+        case 16: //shift
+          shiftKeyIsPressed = true;
+          break;
+        case 75: //k
+          scrollToDiff('top');
+          break;
+        case 74: //j
+          scrollToDiff('bottom');
+          break;
+        case 72: //h
+          changeFile('a.previous');
+          break;
+        case 76: //l
+          changeFile('a.next');
+          break;
       }
     })
     .keyup(function(e) {
       var code = e.keyCode || e.which
-      if (code == '16') {
+      if (code == '16') { //shift
         shiftKeyIsPressed = false;
       }
     })
