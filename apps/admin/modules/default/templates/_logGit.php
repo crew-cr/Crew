3308dafc5933443fd60cbd6a3220a5a55c88f6d0
Denis Roussel <denis.roussel@gmail.com>
2012-06-16 00:01:16 +0200

    git logs date fixed

--- apps/admin/modules/default/templates/_logGit.php
+++ apps/admin/modules/default/templates/_logGit.php
@@ -8,7 +8,7 @@
         <tr class="<?php if($log->getCode()) echo 'error' ?>">
           <td class="code repository_infos tooltip" title="<?php echo $log->getMessage() ?>"><?php echo $log->getCode() ?></td>
           <td class="command"><?php echo $log->getCommand() ?></td>
-          <td class="date"><?php echo $log->getCreatedAt('d/m/Y H:m:i') ?></td>
+          <td class="date"><?php echo $log->getCreatedAt('d/m/Y H:i:s') ?></td>
         </tr>
       <?php endforeach; ?>
     </table>
