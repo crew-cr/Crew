0572fd19fe8992747548406161095d522e00d8e5
Bertrand Jamin <bertrandjamin@free.fr>
2013-04-09 23:04:59 +0200

    API for comments of a review

---
+++ apps/api/modules/comment/actions/listAction.class.php
@@ -0,0 +1,67 @@
+<?php
+ 
+class listAction extends crewAction
+{
+  /**
+* @param sfWebRequest $request
+* @return void
+*/
+  public function execute($request)
+  {
+    
+    $branchName = $request->getParameter('branch');
+    
+    $result = array();
+
+    file_put_contents(
+      sprintf("%s/api.log", sfConfig::get('sf_log_dir')), 
+      sprintf("%s [%s] list comments = reviewId : %s\n", 
+        date('d/m/Y H:i:s'), 
+        $_SERVER['REMOTE_ADDR'], 
+        $branchName), 
+      FILE_APPEND
+    );
+
+
+    $branch = BranchQuery::create()
+      ->filterByName($branchName)
+      ->findOne()
+    ;
+
+    if($branch)
+    {
+      $comments = $branch->getComments();
+      if($comments)
+      {
+        $totalComments = $uncheckedComments = $comments->count();
+        
+        foreach ($comments as $comment) 
+        {
+          //Decrement checked commentt
+          if (null !== $comment->getCheckUserId())
+          {
+              $uncheckedComments--;
+          }
+        }
+        
+        $result = array(
+          'counters'  => array (
+            'total'     => $totalComments,
+            'unchecked' => $uncheckedComments
+          ),
+          'list'  => $comments->toArray()
+        );
+      }
+      $this->getResponse()->setStatusCode('200');
+    }
+    else
+    {
+      $result['message'] = sprintf("No valid branch '%s'", $branchName);
+      $this->getResponse()->setStatusCode('400');
+    }
+
+    $this->getResponse()->setContentType('application/json');
+    return $this->renderText(json_encode($result));
+  }
+}
+
